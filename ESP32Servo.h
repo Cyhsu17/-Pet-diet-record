4824720f91cf2951e4820657e3e78abba73d36c0
AnnHuang-tool <65522888+AnnHuang-tool@users.noreply.github.com>
2021-08-25 17:45:08 +0800

    Update ESP32Servo.h

--- ESP32Servo.h
+++ ESP32Servo.h
@@ -7,13 +7,13 @@ void setup() {
   myservo.attach(13);  // 設定要將伺服馬達接到哪一個PIN腳
 }
 
-void loop() {   
+void loop() { 
+  for(int counter = 0; counter < 6; counter++){  
   myservo.write(0);  //旋轉到0度，就是一般所說的歸零
   delay(1000);
-  myservo.write(180); //旋轉到90度
+  Serial.print("去");
+  myservo.write(180); //旋轉到180度
   delay(1000);
-  myservo.write(0); //旋轉到180度
-  delay(1000);
-  myservo.write(180);
-  delay(1000);
+  Serial.print("回");
+   }
 }
